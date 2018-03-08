5260eb2a52423a005f145b355898d0aa7f459079
Wail Saidani <arakiller6@gmail.com>
2018-03-08 14:36:11 +0100

    add new files

---
+++ data.js
@@ -0,0 +1,512 @@
+var flags = [
+ {
+  "name": "Albania ",
+  "code": "AL",
+  "colors": ["red", "black"]
+ },
+ {
+  "name": "Andorra ",
+  "code": "AD",
+  "colors": ["blue", "yellow", "red"]
+ },
+ {
+  "name": "Austria ",
+  "code": "AT",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Belarus ",
+  "code": "BY",
+  "colors": ["red", "green"]
+ },
+ {
+  "name": "Belgium ",
+  "code": "BE",
+  "colors": ["black", "yellow", "red"]
+ },
+ {
+  "name": "Bosnia and Herzegovina ",
+  "code": "BA",
+  "colors": ["blue", "yellow"]
+ },
+ {
+  "name": "Bulgaria ",
+  "code": "BG",
+  "colors": ["white", "green", "red"]
+ },
+ {
+  "name": "Croatia ",
+  "code": "HR",
+  "colors": ["red", "white", "blue"]
+ },
+ {
+  "name": "Cyprus ",
+  "code": "CY",
+  "colors": ["white"]
+ },
+ {
+  "name": "Czech Republic ",
+  "code": "CZ",
+  "colors": ["blue", "white", "red"]
+ },
+ {
+  "name": "Denmark ",
+  "code": "DK",
+  "colors": ["white", "red"]
+ },
+ {
+  "name": "Estonia ",
+  "code": "EE",
+  "colors": ["blue", "black", "white"]
+ },
+ {
+  "name": "Faroe Islands ",
+  "code": "FO",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Finland ",
+  "code": "FI",
+  "colors": ["blue", "white"]
+ },
+ {
+  "name": "France ",
+  "code": "FR",
+  "colors": ["blue", "white", "red"]
+ },
+ {
+  "name": "Germany ",
+  "code": "DE",
+  "colors": ["black", "red", "yellow"]
+ },
+ {
+  "name": "Gibraltar ",
+  "code": "GI",
+  "colors": ["white", "red"]
+ },
+ {
+  "name": "Greece ",
+  "code": "GR",
+  "colors": ["blue", "white"]
+ },
+ {
+  "name": "Hungary ",
+  "code": "HU",
+  "colors": ["red", "white", "green"]
+ },
+ {
+  "name": "Iceland ",
+  "code": "IS",
+  "colors": ["red", "blue"]
+ },
+ {
+  "name": "Ireland ",
+  "code": "IE",
+  "colors": ["green", "white", "orange"]
+ },
+ {
+  "name": "Isle of Man ",
+  "code": "IM",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Italy ",
+  "code": "IT",
+  "colors": ["green", "white", "red"]
+ },
+ {
+  "name": "Kosovo ",
+  "code": "RS",
+  "colors": ["red", "blue", "white"]
+ },
+ {
+  "name": "Latvia ",
+  "code": "LV",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Liechtenstein ",
+  "code": "LI",
+  "colors": ["blue", "red"]
+ },
+ {
+  "name": "Lithuania ",
+  "code": "LT",
+  "colors": ["yellow", "green", "red"]
+ },
+ {
+  "name": "Luxembourg ",
+  "code": "LU",
+  "colors": ["red", "white", "blue"]
+ },
+ {
+  "name": "Macedonia ",
+  "code": "MK",
+  "colors": ["yellow", "red"]
+ },
+ {
+  "name": "Malta ",
+  "code": "MT",
+  "colors": ["white", "red"]
+ },
+ {
+  "name": "Moldova ",
+  "code": "MD",
+  "colors": ["blue", "yellow", "red"]
+ },
+ {
+  "name": "Monaco ",
+  "code": "MC",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Montenegro ",
+  "code": "ME",
+  "colors": ["red", "yellow"]
+ },
+ {
+  "name": "Netherlands ",
+  "code": "NL",
+  "colors": ["red", "white", "blue"]
+ },
+ {
+  "name": "Norway ",
+  "code": "NO",
+  "colors": ["red", "blue"]
+ },
+ {
+  "name": "Poland ",
+  "code": "PL",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Portugal ",
+  "code": "PT",
+  "colors": ["green", "red"]
+ },
+ {
+  "name": "Romania ",
+  "code": "RO",
+  "colors": ["blue", "yellow", "red"]
+ },
+ {
+  "name": "Russia ",
+  "code": "RU",
+  "colors": ["white", "blue", "red"]
+ },
+ {
+  "name": "San Marino ",
+  "code": "SM",
+  "colors": ["white", "blue"]
+ },
+ {
+  "name": "Serbia ",
+  "code": "RS",
+  "colors": ["red", "blue", "white"]
+ },
+ {
+  "name": "Slovakia ",
+  "code": "SK",
+  "colors": ["red", "blue", "white"]
+ },
+ {
+  "name": "Slovenia ",
+  "code": "SI",
+  "colors": ["red", "blue", "white"]
+ },
+ {
+  "name": "Spain ",
+  "code": "ES",
+  "colors": ["red", "yellow"]
+ },
+ {
+  "name": "Sweden ",
+  "code": "SE",
+  "colors": ["yellow", "blue"]
+ },
+ {
+  "name": "Switzerland ",
+  "code": "CH",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Ukraine ",
+  "code": "UA",
+  "colors": ["blue", "yellow"]
+ },
+ {
+  "name": "United Kingdom ",
+  "code": "GB",
+  "colors": ["red", "blue"]
+ },
+ {
+  "name": "Vatican city ",
+  "code": "VA",
+  "colors": ["yellow", "white"]
+ },
+ {
+  "name": "Yugoslavia ",
+  "code": "RS",
+  "colors": ["red", "blue", "white"]
+ },
+ {
+  "name": "Afghanistan",
+  "code": "AF",
+  "colors": ["black", "red", "green"]
+ },
+ {
+  "name": "Armenia",
+  "code": "AM",
+  "colors": ["red", "blue", "orange"]
+ },
+ {
+  "name": "Azerbaijan",
+  "code": "AZ",
+  "colors": ["blue", "red", "green"]
+ },
+ {
+  "name": "Bahrain",
+  "code": "BH",
+  "colors": ["white", "red"]
+ },
+ {
+  "name": "Bangladesh",
+  "code": "BD",
+  "colors": ["green", "red"]
+ },
+ {
+  "name": "Bhutan",
+  "code": "BT",
+  "colors": ["yellow", "orange"]
+ },
+ {
+  "name": "Brunei",
+  "code": "BN",
+  "colors": ["yellow", "red"]
+ },
+ {
+  "name": "Cambodia",
+  "code": "KH",
+  "colors": ["blue", "red"]
+ },
+ {
+  "name": "China",
+  "code": "CN",
+  "colors": ["red"]
+ },
+ {
+  "name": "Christmas Island",
+  "code": "CX",
+  "colors": ["blue", "green"]
+ },
+ {
+  "name": "Cocos Islands",
+  "code": "CC",
+  "colors": ["green"]
+ },
+ {
+  "name": "Diego Garcia",
+  "code": "IO",
+  "colors": ["blue", "white"]
+ },
+ {
+  "name": "Georgia",
+  "code": "GE",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Hong Kong",
+  "code": "HK",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "India",
+  "code": "IN",
+  "colors": ["orange", "white", "green"]
+ },
+ {
+  "name": "Indonesia",
+  "code": "ID",
+  "colors": ["white", "red"]
+ },
+ {
+  "name": "Iran",
+  "code": "IR",
+  "colors": ["green", "white", "red"]
+ },
+ {
+  "name": "Iraq",
+  "code": "IQ",
+  "colors": ["red", "white", "black"]
+ },
+ {
+  "name": "Israel",
+  "code": "IL",
+  "colors": ["blue", "white"]
+ },
+ {
+  "name": "Japan",
+  "code": "JP",
+  "colors": ["white", "red"]
+ },
+ {
+  "name": "Jordan",
+  "code": "JO",
+  "colors": ["red", "black", "green"]
+ },
+ {
+  "name": "Kazakhstan",
+  "code": "KZ",
+  "colors": ["blue", "yellow"]
+ },
+ {
+  "name": "Kuwait",
+  "code": "KW",
+  "colors": ["black", "green", "red"]
+ },
+ {
+  "name": "Kyrgyzstan",
+  "code": "KG",
+  "colors": ["red"]
+ },
+ {
+  "name": "Laos",
+  "code": "LA",
+  "colors": ["red", "blue"]
+ },
+ {
+  "name": "Lebanon",
+  "code": "LB",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Macau",
+  "code": "MO",
+  "colors": ["green"]
+ },
+ {
+  "name": "Malaysia",
+  "code": "MY",
+  "colors": ["blue", "red", "white"]
+ },
+ {
+  "name": "Maldives",
+  "code": "MV",
+  "colors": ["red", "green"]
+ },
+ {
+  "name": "Mongolia",
+  "code": "MN",
+  "colors": ["red", "blue"]
+ },
+ {
+  "name": "Myanmar",
+  "code": "MM",
+  "colors": ["yellow", "green", "red"]
+ },
+ {
+  "name": "Nepal",
+  "code": "NP",
+  "colors": ["white", "red"]
+ },
+ {
+  "name": "North Korea",
+  "code": "KP",
+  "colors": ["red", "blue", "white"]
+ },
+ {
+  "name": "Oman",
+  "code": "OM",
+  "colors": ["red", "white", "green"]
+ },
+ {
+  "name": "Pakistan",
+  "code": "PK",
+  "colors": ["white", "green"]
+ },
+ {
+  "name": "Palestine",
+  "code": "PS",
+  "colors": ["black", "red", "green"]
+ },
+ {
+  "name": "Philippines",
+  "code": "PH",
+  "colors": ["white", "blue", "red"]
+ },
+ {
+  "name": "Qatar",
+  "code": "QA",
+  "colors": ["white", "red"]
+ },
+ {
+  "name": "Saudi Arabia",
+  "code": "SA",
+  "colors": ["green"]
+ },
+ {
+  "name": "Singapore",
+  "code": "SG",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "South Korea",
+  "code": "KR",
+  "colors": ["white", "red", "blue"]
+ },
+ {
+  "name": "Sri Lanka",
+  "code": "LK",
+  "colors": ["green", "orange", "red"]
+ },
+ {
+  "name": "Syria",
+  "code": "SY",
+  "colors": ["red", "white", "black"]
+ },
+ {
+  "name": "Taiwan",
+  "code": "TW",
+  "colors": ["blue", "red"]
+ },
+ {
+  "name": "Tajikistan",
+  "code": "TJ",
+  "colors": ["red", "white", "green"]
+ },
+ {
+  "name": "Thailand",
+  "code": "TH",
+  "colors": ["red", "white", "blue"]
+ },
+ {
+  "name": "Turkey",
+  "code": "TR",
+  "colors": ["red", "white"]
+ },
+ {
+  "name": "Turkmenistan",
+  "code": "TM",
+  "colors": ["green", "red"]
+ },
+ {
+  "name": "United Arab Emirates",
+  "code": "AE",
+  "colors": ["red", "black", "green", "white"]
+ },
+ {
+  "name": "Uzbekistan",
+  "code": "UZ",
+  "colors": ["blue", "white", "red"]
+ },
+ {
+  "name": "Vietnam",
+  "code": "VN",
+  "colors": ["red", "yellow"]
+ },
+ {
+  "name": "Yemen",
+  "code": "YE",
+  "colors": ["red", "white", "black"]
+ }
+];
